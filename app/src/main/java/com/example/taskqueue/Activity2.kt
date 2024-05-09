c2b2239391a7f2e30d24ca0c8569a9d9c3bcda33
Madara <madarameegama7@gmail.com>
2024-05-09 17:17:27 +0530

    Initial Intents set

--- app/src/main/java/com/example/taskqueue/Activity2.kt
+++ app/src/main/java/com/example/taskqueue/Activity2.kt
@@ -1,28 +1,21 @@
 package com.example.taskqueue
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_task)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Delay the loading of activity_main2.xml after 2 seconds
-        Handler().postDelayed({
-            startActivity(Intent(this, AddTask::class.java))
-            finish()
-        }, 2000) // 2000 milliseconds = 2 seconds
     }
 }
